4286b3ac8f2762f8bf5cd5c81b4548c3aae6d9a7
Elon <11610712@mail.sustc.edu.cn>
2019-05-25 05:43:05 +0800

    single test
    some test

--- sources_1/new/timer.vhd
+++ sources_1/new/timer.vhd
@@ -3,15 +3,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity timer is
 	 Generic( 
 			data_width:integer := 25;
